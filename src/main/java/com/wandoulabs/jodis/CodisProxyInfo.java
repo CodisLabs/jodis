3300b66056642cbf852edb1652093e1a426e9c69
zhangduo <zhangduo@wandoujia.com>
2015-10-14 14:02:10 +0800

    transfer from codis repo

---
+++ src/main/java/com/wandoulabs/jodis/CodisProxyInfo.java
@@ -0,0 +1,56 @@
+/**
+ * @(#)CodisProxyInfo.java, 2015-10-14. 
+ * 
+ * Copyright (c) 2014 Wandoujia Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.wandoulabs.jodis;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * We only care about the proxy address and if it is online.
+ * 
+ * @author Apache9
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CodisProxyInfo {
+
+    private String addr;
+
+    private String state;
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
